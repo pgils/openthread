e03527c37fa998ba58ff3139a7958d8bc319e20f
Pelle van Gils <pelle@pollop.nl>
2018-12-04 13:42:59 +0100

    cli app: Hack in a `toggle led` command

---
+++ include/openthread/platform/toggletest.h
@@ -0,0 +1,17 @@
+#ifndef OPENTHREAD_TOGGLETEST_H_
+#define OPENTHREAD_TOGGLETEST_H_
+
+#include <stdint.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+uint8_t otToggleLed(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // OPENTHREAD_TOGGLETEST_H_
