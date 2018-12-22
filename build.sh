214a3ea7025994bef2428eab5df611bad5dbeeef
Pelle van Gils <pelle@pollop.nl>
2018-12-22 19:23:36 +0100

    build.sh: add an automatic toolchain find function

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+#
+# A toolchain can be specified by setting USE_TOOLCHAIN
 
 MAKEOPT="-f examples/Makefile-nrf52840 USB=1 BOOTLOADER=1"
 FTDFILE="output/nrf52840/bin/ot-shirt-ftd"
@@ -7,10 +9,44 @@ function usage() {
     echo
     echo "  usage: $0 [-c]"
     echo "    c: clean before build."
+    echo "    t: search for toolchain in parent directory."
 }
 
-while getopts ":c" opt; do
+function find_toolchain() {
+    # check if an override has been provided
+    if [[ -n "$USE_TOOLCHAIN" ]]; then
+        return 0
+    fi
+
+    TOOLCHAIN_PATH="$(find ../ -maxdepth 1 -type d -name "*gcc*arm*"|sort|head -1)/bin"
+    if [[ -x "$TOOLCHAIN_PATH"/arm-none-eabi-gcc ]]; then
+        GCC_VER=$("$TOOLCHAIN_PATH"/arm-none-eabi-gcc -dumpversion)
+        GCC_MAJOR=$(echo "$GCC_VER" | sed -E "s/^([[:digit:]]+)\..*/\1/")
+
+        # check for GCC 7 or 8
+        if [[ "$GCC_MAJOR" -lt 9 && "$GCC_MAJOR" -ge 7 ]]; then
+            export PATH="$(realpath $TOOLCHAIN_PATH):$PATH"
+        else
+            echo "Not a valid GCC version for $TOOLCHAIN_PATH: $GCC_MAJOR"
+            return 1
+        fi
+    else
+        echo "No toolchain found."
+        return 1
+    fi
+
+    return 0
+}
+
+while getopts ":tc" opt; do
     case $opt in
+        t)
+        find_toolchain
+        if [[ 0 != $? ]]; then
+            # no toolchain found
+            exit 1
+        fi
+        ;;
         c)
         make $MAKEOPT clean
         if [[ 0 -ne $? ]]; then
@@ -24,6 +60,10 @@ while getopts ":c" opt; do
         ;;
     esac
 done
+
+if [[ -n "$USE_TOOLCHAIN" ]]; then
+    export PATH="$(realpath $USE_TOOLCHAIN/bin):$PATH"
+fi
 
 time make $MAKEOPT
 
